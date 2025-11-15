337036fcdf458a81c73709af307c37264a6534e6
Geovane-Random <geovane.dnk@gmail.com>
2025-11-15 16:06:38 -0300

    refactor: Move a lista de produtos para um arquivo separado
    Move a lista estática de produtos do componente App.js para um novo arquivo data.js. Isso melhora a organização do código e facilita futuras manutenções.

---
+++ data.js
@@ -0,0 +1,74 @@
+export const products = [
+  {
+    id: '1',
+    name: 'Bandeja de isopor PCT',
+    image: './assets/bandeja.png',
+    sizes: ['P', 'M', 'G'] // Variações de tamanho
+  },
+  {
+    id: '2',
+    name: 'Bobina picotada RL',
+    image: './assets/bobina.png',
+    sizes: ['25x35cm', '30x40 cm', '35x45 cm', '40x60 cm']
+  },
+  {
+    id: '3',
+    name: 'Canudos PCT',
+    image: './assets/canudos.png',
+    sizes: ['Médio', 'Longo']
+  },
+  {
+    id: '4',
+    name: 'Colher descartável PCT',
+    image: './assets/colher.png',
+    sizes: ['Único']
+  },
+  {
+    id: '5',
+    name: 'Copo descartável CX',
+    image: './assets/copo.png',
+    sizes: ['180 ml', '200 ml', '300 ml', '350 ml', '400 ml', '500 ml']
+  },
+  {
+    id: '6',
+    name: 'Etiqueta RL',
+    image: './assets/etiqueta.png',
+    sizes: ['Único']
+  },
+  {
+    id: '7',
+    name: 'Fita adesiva RL',
+    image: './assets/fita.png',
+    sizes: ['45 mts', '100 mts']
+  },
+  {
+    id: '8',
+    name: 'Sacos para lixo RL',
+    image: './assets/lixo.png',
+    sizes: ['15 lts', '30 lts', '50 lts', '100 lts', '200 lts']
+  },
+  {
+    id: '9',
+    name: 'Sacola de papel PCT',
+    image: './assets/papel.png',
+    sizes: ['P', 'M', 'G']
+  },
+  {
+    id: '10',
+    name: 'Sacola boca vazada PCT',
+    image: './assets/plast.png',
+    sizes: ['20x30 cm', '25x35 cm', '30x40 cm', '35x45 cm', '40x60 cm']
+  },
+  {
+    id: '11',
+    name: 'Sacos para pipoca PCT',
+    image: './assets/pipoca.png',
+    sizes: ['N°1', 'N°2', 'N°3', 'N°4']
+  },
+  {
+    id: '12',
+    name: 'Saco plástico PP PCT',
+    image: './assets/saco.png',
+    sizes: ['15x20 cm', '20x30 cm', '25x35 cm', '30x40 cm', '35x45 cm', '40x60 cm', '50x80 cm']
+  },
+];
